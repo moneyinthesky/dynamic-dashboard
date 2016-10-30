54b1a343bdf7bcd3ce22f961cb5bc024beeae3b0
Barath Vignarajah <moneyinthesky@gmail.com>
2016-10-30 14:39:15 +0000

    Tweaking dashboard styling

--- src/main/resources/assets/js/dashboards/ApplicationEnvironmentStatus.jsx
+++ src/main/resources/assets/js/dashboards/ApplicationEnvironmentStatus.jsx
@@ -7,7 +7,7 @@ class ApplicationEnvironmentStatus extends React.Component {
 
             var optionalDownStatus = "";
             if(this.props.applicationEnvironmentStatus.nodesDown > 0) {
-                optionalDownStatus = <li className="app-env-status list-group-item"><button type="button" className="btn btn-danger">{this.props.applicationEnvironmentStatus.nodesDown} Down</button></li>;
+                optionalDownStatus = <li className="app-env-status status-down list-group-item"><button type="button" className="btn btn-danger">{this.props.applicationEnvironmentStatus.nodesDown} Down</button></li>;
             }
 
             var versionStatusList = $.map(this.props.applicationEnvironmentStatus.versionToNodeStatusMap, (status, version) => {
@@ -15,7 +15,7 @@ class ApplicationEnvironmentStatus extends React.Component {
                 var unhealthyDependencies = numberOfUnhealthyDependencies>0 ?
                         <button type="button" className="btn btn-danger">{numberOfUnhealthyDependencies + " DD"}</button> : "";
                 return (
-                	<li key={version} className="app-env-status list-group-item">
+                	<li key={version} className="app-env-status status-up list-group-item">
                 	    <div className="container">
                           <div className="col-md-4"><h5>{version}</h5></div>
                           <div className="col-md-4"><button key={version} type="button" className="btn btn-success">{status.nodeCount} Up</button></div>
